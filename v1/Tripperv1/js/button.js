56654efd229b62b6d31b13735b343c7e1d09cff1
GiovanniDeGaetano <147393598+GiovanniDeGaetano@users.noreply.github.com>
2023-11-16 15:59:00 +0100

    Add files via upload

--- v1/Tripperv1/js/button.js
+++ v1/Tripperv1/js/button.js
@@ -1,12 +1,22 @@
 $(function() {
     $("#itinerary").click(function (){
        $(this).hide();
+       newButton();
     });
 });
-/*
-document.addEventListener("DOMContentLoaded", function () {
-    const btn = document.getElementById("itinerary");
-    btn.addEventListener("click",function (){
-       alert("CIAO");
-    });
-});*/
+
+function newButton(){
+    let newButton =[];
+    newButton[0] = document.createElement('button');
+    newButton[0].textContent = 'Eventi';
+    newButton[1] = document.createElement('button');
+    newButton[1].textContent = 'Monumenti';
+    newButton[2] = document.createElement('button');
+    newButton[2].textContent = 'Ristoranti';
+    let br = document.createElement('br');
+    const div = document.getElementsByClassName('button-container')[0];
+    div.appendChild(newButton[0]);
+    div.appendChild(newButton[1]);
+    div.appendChild(br);
+    div.appendChild(newButton[2]);
+}
