6bf7bf54358afed773fd6d90633a94d3e169680c
GiovanniDeGaetano <147393598+GiovanniDeGaetano@users.noreply.github.com>
2023-11-16 14:31:34 +0100

    Add files via upload

---
+++ v1/Tripperv1/js/button.js
@@ -0,0 +1,12 @@
+$(function() {
+    $("#itinerary").click(function (){
+       $(this).hide();
+    });
+});
+/*
+document.addEventListener("DOMContentLoaded", function () {
+    const btn = document.getElementById("itinerary");
+    btn.addEventListener("click",function (){
+       alert("CIAO");
+    });
+});*/
