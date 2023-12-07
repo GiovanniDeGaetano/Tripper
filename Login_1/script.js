e20182abd11396da4949ebcb29abe4298182a9f9
Oneheroday <92482029+Oneheroday@users.noreply.github.com>
2023-12-07 10:57:52 +0100

    Add files via upload

---
+++ Login_1/script.js
@@ -0,0 +1,9 @@
+function showRegistrationForm() {
+    var loginContainer = document.getElementById('login-container');
+    var registrationContainer = document.getElementById('registration-container');
+    
+    // Nascondi il modulo di accesso e mostra il modulo di registrazione
+    loginContainer.style.display = 'none';
+    registrationContainer.style.display = 'block';
+  }
+  
