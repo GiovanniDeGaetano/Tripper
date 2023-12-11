38c2bbdd67ca48d359d218ea0d0a413217fb1f5e
GiovanniDeGaetano <147393598+GiovanniDeGaetano@users.noreply.github.com>
2023-12-11 14:53:09 +0100

    Delete Login_1 directory

--- Login_1/script.js
+++
@@ -1,9 +0,0 @@
-function showRegistrationForm() {
-    var loginContainer = document.getElementById('login-container');
-    var registrationContainer = document.getElementById('registration-container');
-    
-    // Nascondi il modulo di accesso e mostra il modulo di registrazione
-    loginContainer.style.display = 'none';
-    registrationContainer.style.display = 'block';
-  }
-  
