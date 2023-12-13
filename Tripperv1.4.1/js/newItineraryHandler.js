41f1a42ec5db00a76e335879a1b72d0a2031f539
ciro-angarella <148432201+ciro-angarella@users.noreply.github.com>
2023-12-13 14:07:56 +0100

    Add files via upload

---
+++ Tripperv1.4.1/js/newItineraryHandler.js
@@ -0,0 +1,26 @@
+
+// Define options for formatting dates
+let dateOptions ={ year: 'numeric', month: 'long', day: 'numeric' };
+
+// Retrieve stored dates from local storage
+let storedDates = getDataFromLocalStorage("selectedDates");
+
+// Convert start and end date strings to Date objects
+let  startDate = new Date(storedDates.startDate);
+let endDate = new Date(storedDates.endDate);
+
+// Get an array of dates between the start and end dates
+let dates = betweenDates(startDate, endDate);
+
+// Initialize the accordion with the specified ID
+initializeAccordion("#itineraryAccordion");
+
+// Create the accordion with the provided form, dates, and date options
+createAccordion("#itineraryAccordion", dates, dateOptions);
+
+
+
+
+
+
+
